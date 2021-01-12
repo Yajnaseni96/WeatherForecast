aff6a727788e844ab96426ef7557e5a5e9bcc71f
Yajnaseni96 <meyajnasenipanda@gmail.com>
2021-01-12 20:44:24 +0530

    Final commit

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import React, { FC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+
+import { RootState } from './store';
+import Search from './components/Search';
+import Alert from './components/Alert';
+import Weather from './components/Weather';
+import { setAlert } from './store/actions/alertActions';
+import { setError } from './store/actions/weatherActions';
+import styled from 'styled-components';
+import  image1  from './image/image.png';
+
+const Head = styled.div`
+  text-align: center;
+  font-size: 30px;
+  color: black
+`;
+
+const Image = styled.div`
+  background-image: url(${image1});
+  height: 110pc;
+  background-repeat: no-repeat;
+  text-align: center;
+  background-size: contain;
+  display: flex ;
+  flex-direction: column;
+
+  `;
+
+  const Foot = styled.div`
+    text-align: center;
+    font-size: 30px;
+    color: black;
+  `;
+
+const App: FC = () => {
+  const dispatch = useDispatch();
+  const weatherData = useSelector((state: RootState) => state.weather.data);
+  const loading = useSelector((state: RootState) => state.weather.loading);
+  const error = useSelector((state: RootState) => state.weather.error);
+  const alertMsg = useSelector((state: RootState) => state.alert.message);
+
+  return (
+    <>
+    <Image>
+    <Head>
+    
+      <Search title="Enter city name and then press Search!" />
+      {loading ? <Head >Loading...</Head> : weatherData && <Weather data={weatherData} />}
+      {alertMsg && <Alert message={alertMsg} onClose={() => dispatch(setAlert(''))} />}
+      {error && <Alert message={error} onClose={() => dispatch(setError())} />}
+    </Head>
+    </Image>
+    
+    <Foot>Developed by Yajnaseni</Foot>
+    </>
+  );
+}
+
+export default App;
